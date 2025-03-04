b4978359e53243c739da4a1ce8f2d2905c078d3b
AntoanNikolov <antoanameya@gmail.com>
2025-03-04 11:50:41 -0500

    Committed CRUD changes late

--- mysite/TodoList/urls.py
+++ mysite/TodoList/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("", views.TaskListView.as_view(), name="index"),
     # ex: /TodoList/5/
     path("<int:pk>/", views.TaskDetailView.as_view(), name="detail"),
+    path("task_create", views.TaskCreateView.as_view(), name="create"),
+    path("task_update/<int:pk>", views.TaskUpdateView.as_view(), name="update"),
+    path("task_delete/<int:pk>", views.TaskDeleteView.as_view(), name="delete"),
 ]
--- mysite/TodoList/views.py
+++ mysite/TodoList/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 from .models import Task
 # Create your views here.
@@ -10,3 +11,20 @@ class TaskListView(generic.ListView):
 class TaskDetailView(generic.DetailView):
     template_name = "TodoList/task_detail.html"
     model = Task
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    template_name = "TodoList/task_form.html"
+    fields = '__all__'
+    success_url = reverse_lazy('TodoList:index')
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "TodoList/task_delete.html"
+    success_url = reverse_lazy('TodoList:index')
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = "TodoList/task_update.html"
+    fields = '__all__'
+    success_url = reverse_lazy('TodoList:index')
