932f77647dc66c76f733ecb5f181dbdd1aac4604
AntoanNikolov <antoanameya@gmail.com>
2025-03-05 15:51:44 -0500

    Edited urls.py for clarity

--- mysite/TodoList/urls.py
+++ mysite/TodoList/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     # ex: /TodoList/
     path("", views.TaskListView.as_view(), name="index"),
     # ex: /TodoList/5/
-    path("<int:pk>/", views.TaskDetailView.as_view(), name="detail"),
+    path("task/<int:pk>/", views.TaskDetailView.as_view(), name="detail"),
     path("task_create", views.TaskCreateView.as_view(), name="create"),
     path("task_update/<int:pk>", views.TaskUpdateView.as_view(), name="update"),
     path("task_delete/<int:pk>", views.TaskDeleteView.as_view(), name="delete"),
-    path("<int:pk>/comment_create", views.CommentCreateView.as_view(), name='comment_create'),
+    path("task/<int:pk>/comment_create", views.CommentCreateView.as_view(), name='comment_create'),
     path("comment_delete/<int:pk>", views.CommentDeleteView.as_view(), name='comment_delete'), #we only need the comment id here
 ]
